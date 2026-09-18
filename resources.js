344d20385946a5b9ed88495c2cdce882fe88db94
Lukáš <kyselalukas@seznam.cz>
2026-09-18 16:02:57 +0200

    Refresh resource translations

--- resources.js
+++ resources.js
@@ -1,4 +1,4 @@
-import { SUPPORTED_LANGUAGES, applyTranslations, normalizeLanguage, t } from './i18n.js';
+import { SUPPORTED_LANGUAGES, applyTranslations, normalizeLanguage, t } from './i18n.js?v=1.1.5-20260918-sideload2';
 
 const root = document.documentElement;
 const $ = selector => document.querySelector(selector);
