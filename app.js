a449e7bda90479f2bf13062ffd39197c94cc4186
Lukáš <kyselalukas@seznam.cz>
2026-09-18 10:35:30 +0200

    Fix remaining SideStore and icon cache references

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const escapeHtml = value => String(value ?? '').replace(/[&<>'\"]/g, char => ({'
 const tr = key => t(state.lang, key);
 const INSTALLER_ICONS = {
   altstore: 'assets/icons/altstore.svg',
-  sidestore: 'assets/icons/sidestore.svg',
+  sidestore: 'https://sidestore.io/assets/icon.png',
   livecontainer: 'assets/icons/livecontainer.svg'
 };
 function installerIcon(installer) {
