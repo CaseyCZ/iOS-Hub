8ca6567471123123e56ee547248a929a73ff73a4
Lukáš <kyselalukas@seznam.cz>
2026-09-18 10:35:32 +0200

    Fix remaining SideStore and icon cache references

--- builder.js
+++ builder.js
@@ -92,7 +92,7 @@ function tr(key) { return copy[lang()][key] || copy.en[key] || key; }
 function escapeHtml(value) { return String(value ?? '').replace(/[&<>"']/g, c => ({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[c])); }
 const INSTALLER_ICONS = {
   alt: 'assets/icons/altstore.svg',
-  side: 'assets/icons/sidestore.svg',
+  side: 'https://sidestore.io/assets/icon.png',
   live: 'assets/icons/livecontainer.svg'
 };
 function installerIcon(type) {
