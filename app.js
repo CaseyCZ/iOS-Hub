17d2e13197f71f7f04d6484772499081d8eab167
Lukáš <kyselalukas@seznam.cz>
2026-09-18 19:02:32 +0200

    Clarify PAL installer compatibility

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { SUPPORTED_LANGUAGES, applyTranslations, normalizeLanguage, t } from './i18n.js?v=1.1.5-20260918-audit12';
+import { SUPPORTED_LANGUAGES, applyTranslations, normalizeLanguage, t } from './i18n.js?v=1.1.5-20260918-audit13';
 
 const root = document.documentElement;
 const $ = (selector, scope = document) => scope.querySelector(selector);
@@ -243,15 +243,15 @@ function renderSources() {
     const desc = source.description?.[state.lang] || source.description?.en || source.description?.cs || '';
     const checkedOffline = status.online === false && Boolean(status.checkedAt);
     const palOnly = source.mode === 'pal';
-    const altStoreLabel = palOnly ? 'PAL' : 'AltStore';
+    const altStoreLabel = palOnly ? 'AltStore PAL' : 'AltStore';
     const installerButtons = checkedOffline
       ? `<span class="btn small secondary installer-link is-disabled" aria-disabled="true">${installerIcon('altstore')}${altStoreLabel}</span>
         <span class="btn small secondary installer-link is-disabled" aria-disabled="true">${installerIcon('sidestore')}SideStore</span>
         <span class="btn small secondary installer-link is-disabled" aria-disabled="true">${installerIcon('livecontainer')}LiveContainer</span>`
       : palOnly
         ? `<a class="btn small primary installer-link" href="${escapeHtml(sourceInstallerLink('altstore', source))}">${installerIcon('altstore')}${altStoreLabel}</a>
-          <span class="btn small secondary installer-link is-disabled" aria-disabled="true" title="AltStore PAL source">${installerIcon('sidestore')}SideStore</span>
-          <span class="btn small secondary installer-link is-disabled" aria-disabled="true" title="AltStore PAL source">${installerIcon('livecontainer')}LiveContainer</span>`
+          <span class="btn small secondary installer-link is-disabled" aria-disabled="true" title="${escapeHtml(tr('palOnlyInstaller'))}">${installerIcon('sidestore')}SideStore</span>
+          <span class="btn small secondary installer-link is-disabled" aria-disabled="true" title="${escapeHtml(tr('palOnlyInstaller'))}">${installerIcon('livecontainer')}LiveContainer</span>`
         : `<a class="btn small primary installer-link" href="${escapeHtml(sourceInstallerLink('altstore', source))}">${installerIcon('altstore')}${altStoreLabel}</a>
           <a class="btn small secondary installer-link" href="${escapeHtml(sourceInstallerLink('sidestore', source))}">${installerIcon('sidestore')}SideStore</a>
           <a class="btn small secondary installer-link" href="${escapeHtml(sourceInstallerLink('livecontainer', source))}">${installerIcon('livecontainer')}LiveContainer</a>`;
